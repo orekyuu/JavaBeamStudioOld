6dc05ee5002da5e78a7927c4fa4d2755ec65d63c
orekyuu <orekyuu@gmail.com>
2014-10-04 21:32:00 +0900

    テストの修正

--- src/test/java/net/orekyuu/javatter/core/twitter/stream/JavatterStreamImplTest.java
+++ src/test/java/net/orekyuu/javatter/core/twitter/stream/JavatterStreamImplTest.java
@@ -1,7 +1,8 @@
 package net.orekyuu.javatter.core.twitter.stream;
 
-import mockit.Expectations;
-import mockit.Mocked;
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Before;
 import org.junit.Test;
 import twitter4j.DirectMessage;
 import twitter4j.Status;
@@ -12,35 +13,101 @@ import static org.junit.Assert.assertEquals;
 
 public class JavatterStreamImplTest {
 
+    private User user1;
+    private User user2;
+    private Status status1;
+    private Status status2;
+    private DirectMessage directMessage;
+    private UserList userList;
+    private int callCount;
+
+    @Before
+    public void before() {
+        callCount = 0;
+        user1 = new MockUp<User>() {
+            @Mock
+            public long getId() {
+                return 10;
+            }
+
+            @Mock
+            public String getScreenName() {
+                return "test";
+            }
+        }.getMockInstance();
+
+        user2 = new MockUp<User>() {
+            @Mock
+            public long getId() {
+                return 11;
+            }
+
+            @Mock
+            public String getScreenName() {
+                return "test2";
+            }
+        }.getMockInstance();
+
+        status1 = new MockUp<Status>() {
+            @Mock
+            public long getId() {
+                return 10;
+            }
+
+            @Mock
+            public String getText() {
+                return "test";
+            }
+
+        }.getMockInstance();
+
+        status2 = new MockUp<Status>() {
+            @Mock
+            public long getId() {
+                return 20;
+            }
+
+            @Mock
+            public String getText() {
+                return "test2";
+            }
+
+        }.getMockInstance();
+
+        directMessage = new MockUp<DirectMessage>() {
+            @Mock
+            public String getText() {
+                return "test";
+            }
+        }.getMockInstance();
+
+        userList = new MockUp<UserList>() {
+            @Mock
+            public String getName() {
+                return "AAA";
+            }
+        }.getMockInstance();
+    }
+
     @Test
-    public void testOnStatus(@Mocked Status status1, @Mocked Status status2) throws Exception {
-        new Expectations(status1) {{
-            status1.getId();
-            result = 10;
-            status1.getText();
-            result = "test";
-        }};
-        new Expectations(status2) {{
-            status2.getId();
-            result = 11;
-            status2.getText();
-            result = "test2";
-        }};
-
-
+    public void testOnStatus() throws Exception {
         JavatterStreamImpl streamA = new JavatterStreamImpl();
         streamA.addOnStatus(s -> {
             assertEquals(s.getId(), 10);
             assertEquals(s.getText(), "test");
-        });
+            callCount++;
+        }).addOnStatus(s -> callCount++);
         streamA.onStatus(status1);
 
         JavatterStreamImpl streamB = new JavatterStreamImpl();
         streamB.addOnStatus(s -> {
-            assertEquals(s.getId(), 20);
-            assertEquals(s.getText(), "test2");
+            assertEquals(s.getId(), 10);
+            assertEquals(s.getText(), "test");
+            callCount++;
         });
         streamB.onStatus(status2);
+
+        assertEquals(callCount, 3);
     }
 
     @Test
@@ -49,12 +116,19 @@ public class JavatterStreamImplTest {
         Exception e2 = new IllegalAccessException();
 
         JavatterStreamImpl streamA = new JavatterStreamImpl();
-        streamA.addOnException(e -> assertEquals(e.getClass().getName(), NullPointerException.class.getName()));
+        streamA.addOnException(e -> {
+            assertEquals(e.getClass().getName(), NullPointerException.class.getName());
+            callCount++;
+        }).addOnException(e -> callCount++);
         streamA.onException(e1);
 
         JavatterStreamImpl streamB = new JavatterStreamImpl();
-        streamB.addOnException(e -> assertEquals(e.getClass().getName(), IllegalAccessException.class.getName()));
+        streamB.addOnException(e -> {
+            assertEquals(e.getClass().getName(), IllegalAccessException.class.getName());
+            callCount++;
+        });
         streamB.onException(e2);
+        assertEquals(callCount, 3);
     }
 
     @Test
@@ -63,45 +137,39 @@ public class JavatterStreamImplTest {
         streamA.addOnScrubGeo((userID, statusID) -> {
             assertEquals(userID, Long.valueOf(1));
             assertEquals(statusID, Long.valueOf(2));
-        });
+            callCount++;
+        }).addOnScrubGeo((a, b) -> callCount++);
         streamA.onScrubGeo(1, 2);
 
         JavatterStreamImpl streamB = new JavatterStreamImpl();
         streamB.addOnScrubGeo((userID, statusID) -> {
             assertEquals(userID, Long.valueOf(3));
             assertEquals(statusID, Long.valueOf(4));
+            callCount++;
         });
         streamB.onScrubGeo(3, 4);
+        assertEquals(callCount, 3);
     }
 
     @Test
-    public void testOnProfileUpdate(@Mocked User user1, @Mocked User user2) throws Exception {
-        new Expectations(user1) {{
-            user1.getId();
-            result = 10;
-            user1.getScreenName();
-            result = "test";
-        }};
-        new Expectations(user2) {{
-            user2.getId();
-            result = 11;
-            user2.getScreenName();
-            result = "test2";
-        }};
-
+    public void testOnProfileUpdate() throws Exception {
         JavatterStreamImpl streamA = new JavatterStreamImpl();
         streamA.addOnProfileUpdate(s -> {
             assertEquals(s.getId(), 10);
             assertEquals(s.getScreenName(), "test");
-        });
+            callCount++;
+        }).addOnProfileUpdate(a -> callCount++);
         streamA.onUserProfileUpdate(user1);
 
         JavatterStreamImpl streamB = new JavatterStreamImpl();
         streamB.addOnProfileUpdate(s -> {
             assertEquals(s.getId(), 20);
             assertEquals(s.getScreenName(), "test2");
+            callCount++;
         });
         streamB.onUserProfileUpdate(user2);
+
+        assertEquals(callCount, 3);
     }
 
     @Test
@@ -110,152 +178,137 @@ public class JavatterStreamImplTest {
         streamA.addOnDeletionNotice((userID, statusID) -> {
             assertEquals(userID, Long.valueOf(1));
             assertEquals(statusID, Long.valueOf(2));
-        });
+            callCount++;
+        }).addOnDeletionNotice((a, b) -> callCount++);
         streamA.onDeletionNotice(1, 2);
 
         JavatterStreamImpl streamB = new JavatterStreamImpl();
         streamB.addOnDeletionNotice((userID, statusID) -> {
             assertEquals(userID, Long.valueOf(3));
             assertEquals(statusID, Long.valueOf(4));
+            callCount++;
         });
         streamB.onDeletionNotice(3, 4);
+        assertEquals(callCount, 3);
     }
 
     @Test
-    public void testBlockAndUnBlock(@Mocked User user1, @Mocked User user2) throws Exception {
-        new Expectations(user1) {{
-            user1.getScreenName();
-            result = "test";
-        }};
-        new Expectations(user2) {{
-            user2.getScreenName();
-            result = "test2";
-        }};
-
+    public void testBlockAndUnBlock() throws Exception {
         JavatterStreamImpl stream = new JavatterStreamImpl();
         stream.addOnBlockUser((source, target) -> {
             assertEquals(source.getScreenName(), "test");
             assertEquals(target.getScreenName(), "test2");
+            callCount++;
         }).addOnUnBlockUser((source, target) -> {
             assertEquals(source.getScreenName(), "test");
             assertEquals(target.getScreenName(), "test2");
-        });
+            callCount++;
+        }).addOnBlockUser((a, b) -> callCount++).addOnUnBlockUser((a, b) -> callCount++);
 
         stream.onBlock(user1, user2);
         stream.onUnblock(user1, user2);
+        assertEquals(callCount, 4);
     }
 
     @Test
-    public void testFavoriteAndUnFavorite(@Mocked User user1, @Mocked User user2, @Mocked Status status) throws Exception {
-        new Expectations(status) {{
-            status.getId();
-            result = 10;
-            status.getText();
-            result = "test";
-        }};
-        new Expectations(user1) {{
-            user1.getScreenName();
-            result = "test";
-        }};
-        new Expectations(user2) {{
-            user2.getScreenName();
-            result = "test2";
-        }};
-
+    public void testFavoriteAndUnFavorite() throws Exception {
         JavatterStreamImpl stream = new JavatterStreamImpl();
         stream.addOnFavorite((u1, u2, s) -> {
             assertEquals(u1.getScreenName(), "test");
             assertEquals(u2.getScreenName(), "test2");
             assertEquals(s.getId(), 10);
+            callCount++;
         }).addOnUnFavorite((u1, u2, s) -> {
             assertEquals(u1.getScreenName(), "test");
             assertEquals(u2.getScreenName(), "test2");
             assertEquals(s.getId(), 10);
-        });
+            callCount++;
+        }).addOnFavorite((a, b, c) -> callCount++).addOnUnFavorite((a, b, c) -> callCount++);
 
-        stream.onFavorite(user1, user2, status);
-        stream.onUnfavorite(user1, user2, status);
+        stream.onFavorite(user1, user2, status1);
+        stream.onUnfavorite(user1, user2, status1);
+        assertEquals(callCount, 4);
     }
 
     @Test
-    public void testFollowAndUnFollow(@Mocked User user1, @Mocked User user2) throws Exception {
-        new Expectations(user1) {{
-            user1.getScreenName();
-            result = "test";
-        }};
-        new Expectations(user2) {{
-            user2.getScreenName();
-            result = "test2";
-        }};
-
+    public void testFollowAndUnFollow() throws Exception {
         JavatterStreamImpl stream = new JavatterStreamImpl();
         stream.addOnFollow((u1, u2) -> {
             assertEquals(u1.getScreenName(), "test");
             assertEquals(u2.getScreenName(), "test2");
+            callCount++;
         }).addOnUnFollow((u1, u2) -> {
             assertEquals(u1.getScreenName(), "test");
             assertEquals(u2.getScreenName(), "test2");
-        });
+            callCount++;
+        }).addOnFollow((a, b) -> callCount++).addOnUnFollow((a, b) -> callCount++);
 
         stream.onFollow(user1, user2);
         stream.onUnfollow(user1, user2);
+        assertEquals(callCount, 4);
     }
 
     @Test
-    public void testDirectMessage(@Mocked DirectMessage message) throws Exception {
-        new Expectations() {{
-            message.getText();
-            result = "test";
-        }};
-
+    public void testDirectMessage() throws Exception {
         JavatterStreamImpl stream = new JavatterStreamImpl();
-        stream.addOnDirectMessage(m -> assertEquals(m.getText(), "test"));
+        stream.addOnDirectMessage(m -> {
+            assertEquals(m.getText(), "test");
+            callCount++;
+        }).addOnDirectMessage(a -> callCount++);
 
-        stream.onDirectMessage(message);
+        stream.onDirectMessage(directMessage);
+        assertEquals(callCount, 2);
     }
 
     @Test
-    public void testUserList(@Mocked User user1, @Mocked User user2, @Mocked UserList userList) throws Exception {
-        new Expectations(user1) {{
-            user1.getScreenName();
-            result = "test";
-        }};
-        new Expectations(user2) {{
-            user1.getScreenName();
-            result = "test2";
-        }};
-        new Expectations(userList) {{
-            userList.getName();
-            result = "AAA";
-        }};
-
+    public void testUserList() throws Exception {
         JavatterStreamImpl stream = new JavatterStreamImpl();
         stream.addOnUserListCreation((u1, list) -> {
             assertEquals(u1.getScreenName(), "test");
             assertEquals(list.getName(), "AAA");
-        }).addOnUserListDeletion((u1, list) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(list.getName(), "AAA");
-        }).addOnUserListUpdate((u1, u2) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(u2.getScreenName(), "test2");
-        }).addOnUserListSubscripton((u1, u2, list) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(u2.getScreenName(), "test2");
-            assertEquals(list.getName(), "AAA");
-        }).addOnUserListUnsubscripton((u1, u2, list) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(u2.getScreenName(), "test2");
-            assertEquals(list.getName(), "AAA");
-        }).addOnUserMemberAdditon((u1, u2, list) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(u2.getScreenName(), "test2");
-            assertEquals(list.getName(), "AAA");
-        }).addOnUserMemberDeletion((u1, u2, list) -> {
-            assertEquals(u1.getScreenName(), "test");
-            assertEquals(u2.getScreenName(), "test2");
-            assertEquals(list.getName(), "AAA");
-        });
+            callCount++;
+        })
+                .addOnUserListDeletion((u1, list) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(list.getName(), "AAA");
+                    callCount++;
+                })
+                .addOnUserListUpdate((u1, u2) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(u2.getScreenName(), "test2");
+                    callCount++;
+                })
+                .addOnUserListSubscripton((u1, u2, list) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(u2.getScreenName(), "test2");
+                    assertEquals(list.getName(), "AAA");
+                    callCount++;
+                })
+                .addOnUserListUnsubscripton((u1, u2, list) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(u2.getScreenName(), "test2");
+                    assertEquals(list.getName(), "AAA");
+                    callCount++;
+                })
+                .addOnUserMemberAdditon((u1, u2, list) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(u2.getScreenName(), "test2");
+                    assertEquals(list.getName(), "AAA");
+                    callCount++;
+                })
+                .addOnUserMemberDeletion((u1, u2, list) -> {
+                    assertEquals(u1.getScreenName(), "test");
+                    assertEquals(u2.getScreenName(), "test2");
+                    assertEquals(list.getName(), "AAA");
+                    callCount++;
+                })
+                .addOnUserListCreation((u1, list) -> callCount++)
+                .addOnUserListDeletion((u1, list) -> callCount++)
+                .addOnUserListUpdate((u1, u2) -> callCount++)
+                .addOnUserListSubscripton((u1, u2, list) -> callCount++)
+                .addOnUserListUnsubscripton((u1, u2, list) -> callCount++)
+                .addOnUserMemberAdditon((u1, u2, list) -> callCount++)
+                .addOnUserMemberDeletion((u1, u2, list) -> callCount++);
 
         stream.onUserListCreation(user1, userList);
         stream.onUserListDeletion(user1, userList);
@@ -264,5 +317,6 @@ public class JavatterStreamImplTest {
         stream.onUserListUnsubscription(user1, user2, userList);
         stream.onUserListMemberAddition(user1, user2, userList);
         stream.onUserListMemberDeletion(user1, user2, userList);
+        assertEquals(callCount, 14);
     }
 }
