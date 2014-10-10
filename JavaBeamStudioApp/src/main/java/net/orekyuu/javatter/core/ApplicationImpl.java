a2c1e32b2f1f05a9c128b0955b4529d2a6d5b5a4
orekyuu <orekyuu@gmail.com>
2014-10-11 00:04:19 +0900

    メイン画面ののUI大方作成 (cherry picked from commit df9adea)

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/ApplicationImpl.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/ApplicationImpl.java
@@ -1,9 +1,13 @@
 package net.orekyuu.javatter.core;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import net.orekyuu.javatter.api.Application;
 import net.orekyuu.javatter.api.twitter.ClientUserRegister;
 import net.orekyuu.javatter.core.twitter.LocalClientUser;
+
+import java.io.IOException;
 
 public class ApplicationImpl implements Application {
 
@@ -17,13 +21,24 @@ public class ApplicationImpl implements Application {
         loadClientUsers();
     }
 
-    @Override
-    public void onCreate(Stage primaryStage) {
-
-    }
-
 
     private void loadClientUsers() {
         LocalClientUser.loadClientUsers().stream().forEach(ClientUserRegister.getInstance()::registerUser);
     }
+
+    @Override
+    public void onCreate(Stage primaryStage) {
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            Scene scene = new Scene(loader.load(getClass().getResourceAsStream("root.fxml")));
+            MainWindowPresenter presenter = loader.getController();
+            primaryStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Javaビーム工房");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
 }
