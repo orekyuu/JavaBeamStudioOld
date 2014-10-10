e2ad5e163a213ddf4787bfcb77fe81cfa59e251d
orekyuu <orekyuu@gmail.com>
2014-10-10 19:45:48 +0900

    メソッドを抽出

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/Main.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/Main.java
@@ -1,23 +1,26 @@
-package net.orekyuu.javatter.core; 
+package net.orekyuu.javatter.core;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import net.orekyuu.javatter.api.GlobalAccess;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
 
-import net.orekyuu.javatter.api.GlobalAccess;
-
 public class Main extends Application {
-	@Override
-	public void start(Stage stage) throws Exception {
-		Class<GlobalAccess> clazz = GlobalAccess.class;
-		Field f= clazz.getDeclaredField("application"); 
-		f.setAccessible(true);
-		f.set(GlobalAccess.getInstance(), new ApplicationImpl());		
-	}
+    @Override
+    public void start(Stage stage) throws Exception {
+        setupGrobalAccess();
+    }
 
-	public static void main(String[] args) throws IOException {
-		launch(args);
-	}
+    private void setupGrobalAccess() throws ReflectiveOperationException {
+        Class<GlobalAccess> clazz = GlobalAccess.class;
+        Field f = clazz.getDeclaredField("application");
+        f.setAccessible(true);
+        f.set(GlobalAccess.getInstance(), new ApplicationImpl());
+    }
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+    }
 }
