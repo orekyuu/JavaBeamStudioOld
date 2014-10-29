3b6842f1c3f983b115724688581cb39b1d79bb2a
orekyuu <orekyuu@gmail.com>
2014-10-29 20:37:05 +0900

    TreeViewに項目を追加

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/config/ConfigPresenter.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/config/ConfigPresenter.java
@@ -3,6 +3,8 @@ package net.orekyuu.javatter.core.config;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import net.orekyuu.javatter.core.control.ScreenController;
 
 import java.net.URL;
@@ -14,11 +16,26 @@ import java.util.ResourceBundle;
 public class ConfigPresenter implements Initializable {
 
     @FXML
+    private TreeView<String> tree;
+    @FXML
     private ScreenController screen;
     @FXML
     private TextField searchField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        TreeItem<String> root = new TreeItem<>();
+        root.getChildren().add(new TreeItem<>("基本設定"));
+        root.getChildren().add(new TreeItem<>("アカウント"));
+        tree.setRoot(root);
+        tree.setShowRoot(false);
+
+        screen.loadScreen("基本設定", "Account.fxml");
+        screen.loadScreen("アカウント", "Account.fxml");
+
+        tree.getSelectionModel().selectedItemProperty().addListener(e -> {
+            String name = tree.getSelectionModel().getSelectedItem().getValue();
+            screen.setScreen(name);
+        });
     }
 }
