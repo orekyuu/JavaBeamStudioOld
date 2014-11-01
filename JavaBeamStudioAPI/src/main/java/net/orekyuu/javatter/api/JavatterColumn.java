acb6250a8c88ac422b0df80135d4569f664787ae
serori <seroriKETC@seroriKETC-PC>
2014-11-01 23:31:03 +0900

    JavatterColumnインターフェイスを実装しました

---
+++ JavaBeamStudioAPI/src/main/java/net/orekyuu/javatter/api/JavatterColumn.java
@@ -0,0 +1,11 @@
+package net.orekyuu.javatter.api;
+
+import net.orekyuu.javatter.api.twitter.ClientUser;
+
+public interface JavatterColumn {
+	/**
+	 * クライアントユーザー
+	 * @param clientUser
+	 */
+	void setClientUser(ClientUser clientUser);
+}
