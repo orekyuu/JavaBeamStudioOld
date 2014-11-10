582d05a4bc0bf11b9ac9514fddfac98311037180
serori <seroriKETC@seroriKETC-PC>
2014-11-10 23:50:21 +0900

    addOnstatusの中で非同期処理をするように修正しました。

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/UserStreamController.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/UserStreamController.java
@@ -3,7 +3,11 @@ package net.orekyuu.javatter.core;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+
 import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -16,26 +20,21 @@ import twitter4j.Status;
  *
  */
 public class UserStreamController implements JavatterColumn,Initializable {
-	// セルがまだわからないので仮組みです。
 	@FXML
 	private ListView<Status> userStreamList;
-	/*
-	private ObservableList<String> observableList =
-			FXCollections.observableArrayList();
-	*/
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO 自動生成されたメソッド・スタブ
 		userStreamList.setCellFactory(cell->new TweetCell());
 	}
 	@Override
 	public void setClientUser(ClientUser clientUser) {
-		// UI側で非同期処理
-		Platform.runLater(() -> {
-			// Streamを取得し、ステータスを追加する
-			clientUser.getStream().addOnStatus(userStreamList.getItems()::add);
-		});
+			
+		clientUser.getStream().addOnStatus(status ->{
+            Platform.runLater(() -> {
+				userStreamList.getItems().add(status);
+            });
+        });
 	}
-
+	
 
 }
