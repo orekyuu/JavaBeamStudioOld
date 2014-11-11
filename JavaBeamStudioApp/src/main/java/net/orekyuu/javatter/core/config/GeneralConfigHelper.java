73f2051cbb06f4a0059c7d03833663b25f1cdfcc
orekyuu <orekyuu@yahoo.co.jp>
2014-11-11 17:01:49 +0900

    コンフィグ情報の扱い方を変更

---
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/config/GeneralConfigHelper.java
@@ -0,0 +1,137 @@
+package net.orekyuu.javatter.core.config;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.table.DatabaseTable;
+import com.j256.ormlite.table.TableUtils;
+import net.orekyuu.javatter.core.dialog.ExceptionDialogBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 一般設定を扱うためのヘルパークラスです。
+ */
+public class GeneralConfigHelper {
+
+    private GeneralConfigHelper() {
+        //インスタンス化抑制
+        throw new AssertionError();
+    }
+
+    /**
+     * データベースから一般設定の情報を読み込みます。
+     * @return 読み込んだコンフィグの情報
+     */
+    public static GeneralConfigModel loadConfigFromDB() {
+        List<GeneralConfig> configs = loadConfigTableFromDB();
+        Map<String, String> map = configs.stream()
+                .collect(Collectors.toMap(GeneralConfig::getConfigName, GeneralConfig::getData));
+        return GeneralConfigModel.create(map);
+    }
+
+    private static List<GeneralConfig> loadConfigTableFromDB() {
+        JdbcPooledConnectionSource connectionSource = null;
+        try {
+            makeFile();
+            connectionSource = new JdbcPooledConnectionSource("jdbc:sqlite:" + ConfigPresenter.db_name);
+            Dao<GeneralConfig, Long> dao = DaoManager.createDao(connectionSource, GeneralConfig.class);
+            TableUtils.createTableIfNotExists(connectionSource, GeneralConfig.class);
+            QueryBuilder<GeneralConfig, Long> queryBuilder = dao.queryBuilder();
+            if (dao.countOf() == 0) {
+                //デフォルトを保存してからもう一度ロードする
+                saveToDB(new GeneralConfigModel());
+                return loadConfigTableFromDB();
+            }
+            return queryBuilder.query();
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            ExceptionDialogBuilder.create(e);
+        } finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (SQLException e) {
+                    ExceptionDialogBuilder.create(e);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 一般設定をデータベースに保存します。
+     * @param model 一般設定の情報
+     */
+    public static void saveToDB(GeneralConfigModel model) {
+        JdbcPooledConnectionSource connectionSource = null;
+        List<GeneralConfig> configs = toGeneralConfigList(model);
+        try {
+            makeFile();
+            connectionSource = new JdbcPooledConnectionSource("jdbc:sqlite:" + ConfigPresenter.db_name);
+            Dao<GeneralConfig, ?> dao = DaoManager.createDao(connectionSource, GeneralConfig.class);
+            TableUtils.createTableIfNotExists(connectionSource, GeneralConfig.class);
+            for (GeneralConfig config : configs) {
+                dao.createOrUpdate(config);
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            ExceptionDialogBuilder.create(e);
+        } finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (SQLException e) {
+                    ExceptionDialogBuilder.create(e);
+                }
+            }
+        }
+    }
+
+    private static List<GeneralConfig> toGeneralConfigList(GeneralConfigModel model) {
+        Field[] fields = GeneralConfigModel.class.getDeclaredFields();
+        List<GeneralConfig> configs = new LinkedList<>();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            GeneralConfig generalConfig = new GeneralConfig();
+            generalConfig.configName = field.getName();
+            try {
+                generalConfig.data = String.valueOf(field.get(model));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            configs.add(generalConfig);
+        }
+        return configs;
+    }
+
+    private static void makeFile() throws IOException {
+        File file = new File(ConfigPresenter.db_name);
+        if (!file.exists()) file.createNewFile();
+    }
+
+    @DatabaseTable(tableName = "general_config")
+    private static class GeneralConfig {
+        @DatabaseField(canBeNull = false, id = true)
+        private String configName;
+        @DatabaseField(canBeNull = true)
+        private String data;
+
+        private String getConfigName() {
+            return configName;
+        }
+
+        private String getData() {
+            return data;
+        }
+    }
+}
---
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/config/GeneralConfigModel.java
@@ -0,0 +1,116 @@
+package net.orekyuu.javatter.core.config;
+
+import java.util.Map;
+
+/**
+ * 一般設定の情報
+ */
+public class GeneralConfigModel {
+    private boolean checkTweet = false;
+    private boolean checkReply = false;
+    private boolean checkRT = false;
+    private boolean checkFav = false;
+    private String nameDisplayType = NameDisplayType.ID_NAME.name();
+    private boolean isExpandURL = true;
+
+    public boolean isCheckTweet() {
+        return checkTweet;
+    }
+
+    public boolean isCheckReply() {
+        return checkReply;
+    }
+
+    public boolean isCheckRT() {
+        return checkRT;
+    }
+
+    public boolean isCheckFav() {
+        return checkFav;
+    }
+
+    public String getNameDisplayType() {
+        return nameDisplayType;
+    }
+
+    public boolean isExpandURL() {
+        return isExpandURL;
+    }
+
+    public void setCheckTweet(boolean checkTweet) {
+        this.checkTweet = checkTweet;
+    }
+
+    public void setCheckReply(boolean checkReply) {
+        this.checkReply = checkReply;
+    }
+
+    public void setCheckRT(boolean checkRT) {
+        this.checkRT = checkRT;
+    }
+
+    public void setCheckFav(boolean checkFav) {
+        this.checkFav = checkFav;
+    }
+
+    public void setNameDisplayType(String nameDisplayType) {
+        this.nameDisplayType = nameDisplayType;
+    }
+
+    public void setExpandURL(boolean isExpandURL) {
+        this.isExpandURL = isExpandURL;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("GeneralConfigModel{");
+        sb.append("checkTweet=").append(checkTweet);
+        sb.append(", checkReply=").append(checkReply);
+        sb.append(", checkRT=").append(checkRT);
+        sb.append(", checkFav=").append(checkFav);
+        sb.append(", nameDisplayType='").append(nameDisplayType).append('\'');
+        sb.append(", isExpandURL=").append(isExpandURL);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GeneralConfigModel that = (GeneralConfigModel) o;
+
+        if (checkFav != that.checkFav) return false;
+        if (checkRT != that.checkRT) return false;
+        if (checkReply != that.checkReply) return false;
+        if (checkTweet != that.checkTweet) return false;
+        if (isExpandURL != that.isExpandURL) return false;
+        if (nameDisplayType != null ? !nameDisplayType.equals(that.nameDisplayType) : that.nameDisplayType != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (checkTweet ? 1 : 0);
+        result = 31 * result + (checkReply ? 1 : 0);
+        result = 31 * result + (checkRT ? 1 : 0);
+        result = 31 * result + (checkFav ? 1 : 0);
+        result = 31 * result + (nameDisplayType != null ? nameDisplayType.hashCode() : 0);
+        result = 31 * result + (isExpandURL ? 1 : 0);
+        return result;
+    }
+
+    static GeneralConfigModel create(Map<String, String> map) {
+        GeneralConfigModel model = new GeneralConfigModel();
+        model.checkFav = Boolean.valueOf(map.getOrDefault("checkFav", String.valueOf(model.checkFav)));
+        model.checkReply = Boolean.valueOf(map.getOrDefault("checkReply", String.valueOf(model.checkReply)));
+        model.checkRT = Boolean.valueOf(map.getOrDefault("checkRT", String.valueOf(model.checkRT)));
+        model.checkTweet = Boolean.valueOf(map.getOrDefault("checkTweet", String.valueOf(model.checkTweet)));
+        model.isExpandURL = Boolean.valueOf(map.getOrDefault("isExpandURL", String.valueOf(model.isExpandURL)));
+        model.nameDisplayType = map.getOrDefault("nameDisplayType", model.nameDisplayType);
+        return model;
+    }
+}
