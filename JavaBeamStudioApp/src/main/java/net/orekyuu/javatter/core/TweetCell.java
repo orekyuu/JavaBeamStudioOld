e0fc37d5fbcbbd38583af49ec730e92ea87584c5
Qwerty-Zenkun <qwerty_zenkun_3732@yahoo.co.jp>
2014-11-12 22:09:32 +0900

    インデントの修正

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/TweetCell.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/TweetCell.java
@@ -8,43 +8,43 @@ import javafx.scene.control.ListCell;
 import twitter4j.Status;
 
 public class TweetCell extends ListCell<Status> {
-	/**
-	 * 所定コントローラ
-	 */
-	private TweetCellController tweetCellController;
+    /**
+     * 所定コントローラ
+     */
+    private TweetCellController tweetCellController;
 
-	/**
-	 * アイテムの内容をStatusに従って切り替える
-	 *
-	 * @param status
-	 *            受け取ったステータス
-	 * @param empty
-	 *            空かどうか
-	 */
-	@Override
-	protected void updateItem(Status status, boolean empty) {
-		// スーパークラスから必要な機能を継承
-		super.updateItem(status, empty);
-		if (empty) {
-			// 空の場合は表示・描画を行わない
-			setText(null);
-			setGraphic(null);
-			tweetCellController = null;
-		} else {
-			// 空でない場合は
-			// 名前の取得と表示
-			if (getGraphic() != null) {
-				FXMLLoader fxmlLoader = new FXMLLoader();
-				try {
-					Parent parent = (Parent) fxmlLoader.load(Main.class
-							.getResourceAsStream("tweetcell.fxml"));
-					setGraphic(parent);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				tweetCellController = fxmlLoader.getController();
-			}
-			tweetCellController.updateTweetCell(status);
-		}
-	}
+    /**
+     * アイテムの内容をStatusに従って切り替える
+     *
+     * @param status
+     *            受け取ったステータス
+     * @param empty
+     *            空かどうか
+     */
+    @Override
+    protected void updateItem(Status status, boolean empty) {
+        // スーパークラスから必要な機能を継承
+        super.updateItem(status, empty);
+        if (empty) {
+            // 空の場合は表示・描画を行わない
+            setText(null);
+            setGraphic(null);
+            tweetCellController = null;
+        } else {
+            // 空でない場合は
+            // 名前の取得と表示
+            if (getGraphic() != null) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                try {
+                    Parent parent = (Parent) fxmlLoader.load(Main.class
+                            .getResourceAsStream("tweetcell.fxml"));
+                    setGraphic(parent);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                tweetCellController = fxmlLoader.getController();
+            }
+            tweetCellController.updateTweetCell(status);
+        }
+    }
 }
--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/TweetCellController.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/TweetCellController.java
@@ -19,49 +19,49 @@ import twitter4j.User;
 
 public class TweetCellController implements Initializable {
 
-	@FXML
-	private Label screen_name;
-	@FXML
-	private Label name;
-	@FXML
-	private Label time;
-	@FXML
-	private Label tweet_sentence;
-	@FXML
-	private ImageView profileimage;
-	@FXML
-	private TextFlow caption;
+    @FXML
+    private Label screen_name;
+    @FXML
+    private Label name;
+    @FXML
+    private Label time;
+    @FXML
+    private Label tweet_sentence;
+    @FXML
+    private ImageView profileimage;
+    @FXML
+    private TextFlow caption;
 
-	/**
-	 * timeラベル用の時刻フォーマット
-	 */
-	private static final String DATE_TIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
+    /**
+     * timeラベル用の時刻フォーマット
+     */
+    private static final String DATE_TIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// 特になし
-	}
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // 特になし
+    }
 
-	/**
-	 * アイテムの内容をStatusに従って切り替える
-	 *
-	 * @param status
-	 *            受け取ったステータス
-	 */
-	public void updateTweetCell(Status status) {
-		User user = status.getUser();
-		screen_name.setText(user.getScreenName());
-		name.setText(user.getName());
+    /**
+     * アイテムの内容をStatusに従って切り替える
+     *
+     * @param status
+     *            受け取ったステータス
+     */
+    public void updateTweetCell(Status status) {
+        User user = status.getUser();
+        screen_name.setText(user.getScreenName());
+        name.setText(user.getName());
 
-		// 時間の取得と表示
-		ZonedDateTime ztd = status.getCreatedAt().toInstant()
-				.atZone(ZoneId.systemDefault());
-		time.setText(ztd.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
-		tweet_sentence.setText(status.getText());
-		// イメージ設定(非同期処理)
-		CompletableFuture.runAsync(() -> {
-		    Image img = new Image(user.getProfileImageURL());
-		    Platform.runLater(() -> profileimage.setImage(img));
-		});
-	}
+        // 時間の取得と表示
+        ZonedDateTime ztd = status.getCreatedAt().toInstant()
+                .atZone(ZoneId.systemDefault());
+        time.setText(ztd.format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+        tweet_sentence.setText(status.getText());
+        // イメージ設定(非同期処理)
+        CompletableFuture.runAsync(() -> {
+            Image img = new Image(user.getProfileImageURL());
+            Platform.runLater(() -> profileimage.setImage(img));
+        });
+    }
 }
