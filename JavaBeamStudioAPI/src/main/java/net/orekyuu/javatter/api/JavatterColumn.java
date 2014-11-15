97a4c6ab65456f599511f478a091c61ed2393a6f
serori <seroriKETC@seroriKETC-PC>
2014-11-15 17:57:23 +0900

    JavaDocを追加

--- JavaBeamStudioAPI/src/main/java/net/orekyuu/javatter/api/JavatterColumn.java
+++ JavaBeamStudioAPI/src/main/java/net/orekyuu/javatter/api/JavatterColumn.java
@@ -1,10 +1,14 @@
 package net.orekyuu.javatter.api;
 
 import net.orekyuu.javatter.api.twitter.ClientUser;
-
+/**
+ * カラムを表すインターフェース
+ *
+ */
 public interface JavatterColumn {
 	/**
 	 * クライアントユーザー
+	 * 
 	 * @param clientUser
 	 */
 	void setClientUser(ClientUser clientUser);
