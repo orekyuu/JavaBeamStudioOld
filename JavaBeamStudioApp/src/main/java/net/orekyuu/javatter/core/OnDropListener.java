ccc53cc22f06c34303364241b4418bacc24fad00
seriori <seriori@orekyuu.net>
2014-11-19 15:51:34 +0900

    D&Dに関するインターフェースを作成

---
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/OnDropListener.java
@@ -0,0 +1,20 @@
+package net.orekyuu.javatter.core;
+
+import javafx.scene.input.DragEvent;
+
+/**
+ *画像のD&Dに関する 
+ *
+ */
+public interface OnDropListener {
+	/**
+	 * ドロップ前のイベント
+	 * @param e イベント
+	 */
+	void onDrop(DragEvent e);
+	/**
+	 * ドロップ後のイベント
+	 * @param e イベント
+	 */
+	void onDroped(DragEvent e);
+}
