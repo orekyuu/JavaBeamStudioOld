c20aa285b32779717457e7cc91b38e2ce819d36c
seriori <seriori@orekyuu.net>
2014-11-19 15:49:09 +0900

    CustomTextAreaにonDoropメソッドのとonDropedのメソッドのコールバックを追加しました

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/CustomTextArea.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/CustomTextArea.java
@@ -2,24 +2,42 @@ package net.orekyuu.javatter.core;
 
 import net.orekyuu.javatter.api.EditText;
 import javafx.scene.control.TextArea;
+
 /**
- *カスタムされたTextAreaです。 
+ * カスタムされたTextAreaです。
  */
-public class CustomTextArea extends TextArea{
+public class CustomTextArea extends TextArea {
 	public CustomTextArea() {
 		super();
 	}
+
 	private EditText editTextCallBack;
+	private OnDropListener onDropCallBack;
+
 	/**
 	 * コールバックするオブジェクトを指定します。
-	 * @param o EditTextを継承したオブジェクト
+	 * 
+	 * @param o
+	 *            EditTextを継承したオブジェクト
 	 */
-	public void addChangeTextListener(Object o){
-		if(o instanceof EditText){
+	public void addChangeTextListener(Object o) {
+		if (o instanceof EditText) {
 			editTextCallBack = (EditText) o;
+			this.textProperty().addListener((s1, s2, s3) -> {
+				editTextCallBack.onChanged(getText(), getText().length());
+			});
 		}
-		this.textProperty().addListener((s1,s2,s3)->{
-			editTextCallBack.onChanged(getText(), getText().length());
-		});
+	}
+
+	public void addOnDropListener(Object o) {
+		if (o instanceof OnDropListener) {
+			onDropCallBack = (OnDropListener) o;
+			this.setOnDragOver(e->{
+				onDropCallBack.onDrop(e);
+			});
+			this.setOnDragDropped(e->{
+				onDropCallBack.onDroped(e);
+			});
+		}
 	}
 }
