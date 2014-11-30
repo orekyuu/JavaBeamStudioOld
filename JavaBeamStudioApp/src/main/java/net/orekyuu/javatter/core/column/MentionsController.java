5d3d08164464cdf98ef2d5ed4ecfdf341eafb788
serori <seroriKETC@seroriKETC-PC>
2014-11-30 22:28:18 +0900

    userIdを-1かどうかチェックするif文を取り除きました

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/MentionsController.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/MentionsController.java
@@ -11,26 +11,26 @@ import net.orekyuu.javatter.core.models.StatusModel;
 public class MentionsController implements JavatterColumn {
     @FXML
     ListView<StatusModel> mentionsList;
-    private long userId = -1;
+    private long userId;
 
     @Override
     public void setClientUser(ClientUser clientUser) {
-        if (userId == -1) {
-            try {
-                userId = clientUser.getTwitter().getId();
-            } catch (IllegalStateException | TwitterException e) {
-                e.printStackTrace();
-            }
+        try {
+            userId = clientUser.getTwitter().getId();
+        } catch (IllegalStateException | TwitterException e) {
+            e.printStackTrace();
         }
         mentionsList.setCellFactory(cell -> new TweetCell(clientUser));
-        clientUser.getStream().addOnStatus(status -> {
-            if (status.getInReplyToUserId() == userId) {
-                Platform.runLater(() -> {
-                    mentionsList.getItems().add(0, StatusModel.Builder.build(status));
-                    if (mentionsList.getItems().size() > 100)
-                        mentionsList.getItems().remove(100);
+        clientUser.getStream().addOnStatus(
+                status -> {
+                    if (status.getInReplyToUserId() == userId) {
+                        Platform.runLater(() -> {
+                            mentionsList.getItems().add(0,
+                                    StatusModel.Builder.build(status));
+                            if (mentionsList.getItems().size() > 100)
+                                mentionsList.getItems().remove(100);
+                        });
+                    }
                 });
-            }
-        });
     }
 }
