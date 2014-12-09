4ba9f057ce64146c150e200c87ad3a10f32b1877
Qwerty-Zenkun <qwerty_zenkun_3732@yahoo.co.jp>
2014-12-10 00:21:54 +0900

    タイムライン・メンションを読み込んで、表示されるように設定

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/MentionsController.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/MentionsController.java
@@ -1,21 +1,38 @@
 package net.orekyuu.javatter.core.column;
 
-import twitter4j.TwitterException;
+import java.util.List;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import net.orekyuu.javatter.api.JavatterColumn;
 import net.orekyuu.javatter.api.twitter.ClientUser;
 import net.orekyuu.javatter.api.models.StatusModel;
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 public class MentionsController implements JavatterColumn {
     @FXML
     ListView<StatusModel> mentionsList;
+    private final int LIMIT_VALUE = 30;
 
     @Override
     public void setClientUser(ClientUser clientUser) {
         long userId = getUserId(clientUser);
         mentionsList.setCellFactory(cell -> new TweetCell(clientUser));
+        Platform.runLater(() -> {
+            List<Status> homeTimeline = null;
+            try {
+                homeTimeline = clientUser.getTwitter().getHomeTimeline();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            homeTimeline.stream().limit(LIMIT_VALUE).forEachOrdered(status -> {
+                mentionsList.getItems().add(StatusModel.Builder.build(status));
+            });
+        });
+        mentionsList.setCellFactory(cell -> new TweetCell(clientUser));
+
         clientUser.getStream().addOnStatus(
                 status -> {
                     if (status.getInReplyToUserId() == userId) {
--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/UserStreamController.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/column/UserStreamController.java
@@ -1,11 +1,14 @@
 package net.orekyuu.javatter.core.column;
 
+import java.util.List;
+
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import net.orekyuu.javatter.api.JavatterColumn;
 import net.orekyuu.javatter.api.twitter.ClientUser;
 import net.orekyuu.javatter.api.models.StatusModel;
+import twitter4j.Status;
 
 /**
  * userstreamのコントローラです。
@@ -13,16 +16,36 @@ import net.orekyuu.javatter.api.models.StatusModel;
 public class UserStreamController implements JavatterColumn {
     @FXML
     private ListView<StatusModel> userStreamList;
+    private final int LIMIT_VALUE = 30;
 
     @Override
     public void setClientUser(ClientUser clientUser) {
         userStreamList.setCellFactory(cell -> new TweetCell(clientUser));
-        clientUser.getStream().addOnStatus(status -> {
-            Platform.runLater(() -> {
-                userStreamList.getItems().add(0, StatusModel.Builder.build(status));
-                if(userStreamList.getItems().size() > 100)
-                    userStreamList.getItems().remove(100);
-            });
+        Platform.runLater(() -> {
+            List<Status> homeTimeline = null;
+            try {
+                homeTimeline = clientUser.getTwitter().getHomeTimeline();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            homeTimeline
+                    .stream()
+                    .limit(LIMIT_VALUE)
+                    .forEachOrdered(
+                            status -> {
+                                userStreamList.getItems().add(
+                                        StatusModel.Builder.build(status));
+                            });
         });
+
+        clientUser.getStream().addOnStatus(
+                status -> {
+                    Platform.runLater(() -> {
+                        userStreamList.getItems().add(0,
+                                StatusModel.Builder.build(status));
+                        if (userStreamList.getItems().size() > 100)
+                            userStreamList.getItems().remove(100);
+                    });
+                });
     }
 }
