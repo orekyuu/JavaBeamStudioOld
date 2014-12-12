1b0aafa2826905c843c62978082e6c24327c8959
serori <seroriKETC@seroriKETC-PC>
2014-12-13 02:45:47 +0900

    save()とcancel()の中身を実装しました

--- JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/notification/NotificationConfigPresenter.java
+++ JavaBeamStudioApp/src/main/java/net/orekyuu/javatter/core/notification/NotificationConfigPresenter.java
@@ -3,8 +3,6 @@ package net.orekyuu.javatter.core.notification;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.fxml.FXML;
@@ -36,14 +34,13 @@ public class NotificationConfigPresenter implements Initializable {
     private ListView<NotificationTypeManager.NotificationConfig> notificationList;
     
     private ListProperty<NotificationTypeManager.NotificationConfig> currentConfigProperty = new SimpleListProperty<>();
-    private List<NotificationTypeManager.NotificationConfig> previousConfig = new ArrayList<>();
-    private BooleanProperty toggleButtonDisableProperty = new SimpleBooleanProperty(false);
+    private List<BooleanProperty> toggleButtonSelectedproperties = new ArrayList<>();
+    private List<Boolean> previousConfig = new ArrayList<>();
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+  
         Bindings.bindBidirectional(volumeText.textProperty(), volumeSlider.valueProperty(), new NumberStringConverter());
         NotificationTypeManager typeManager = (NotificationTypeManager) GlobalAccess.getInstance().getNotificationTypeRegister();
-        previousConfig = notificationList.getItems();
-        currentConfigProperty.bind(notificationList.itemsProperty());
         notificationList.setCellFactory(param -> new ListCell<NotificationTypeManager.NotificationConfig>() {
             @Override
             protected void updateItem(NotificationTypeManager.NotificationConfig item, boolean empty) {
@@ -53,21 +50,23 @@ public class NotificationConfigPresenter implements Initializable {
                 } else {
                     Text text = new Text(item.getNotificationType());
                     ToggleButton toggleButton = new ToggleButton(item.isNotice() ? "通知する" : "通知しない");
-                    toggleButton.disableProperty().bind(toggleButtonDisableProperty);
                     toggleButton.setSelected(item.isNotice());
+                    previousConfig.add(item.isNotice());
+                    System.out.println(item.getNotificationType()+":"+item.isNotice());
+                    BooleanProperty toggleButtonSelectedProperty = toggleButton.selectedProperty();
+                    toggleButtonSelectedproperties.add(toggleButtonSelectedProperty);
                     toggleButton.textProperty().bind(Bindings.when(toggleButton.selectedProperty()).then("通知する").otherwise("通知しない"));
                     toggleButton.selectedProperty().addListener(e ->{ 
                         item.setNotice(toggleButton.isSelected());
                     });
-                    
                     Pane grow = new Pane();
                     HBox.setHgrow(grow, Priority.ALWAYS);
                     setGraphic(new HBox(text, grow, toggleButton));
                 }
             }
-        });
-
+        });       
         typeManager.getConfigs().forEach(notificationList.getItems()::add);
+        currentConfigProperty.bind(notificationList.itemsProperty());
         typeManager.getConfigs().forEach(conf -> System.out.println(conf.getNotificationType()));
     }
 
@@ -76,13 +75,17 @@ public class NotificationConfigPresenter implements Initializable {
     }
 
     public void save() {
-        toggleButtonDisableProperty.set(true);
         NotificationTypeManager typeManager = (NotificationTypeManager) GlobalAccess.getInstance().getNotificationTypeRegister();
         typeManager.saveNotificationConfigs(currentConfigProperty);
-        toggleButtonDisableProperty.set(false);
+        currentConfigProperty.forEach(p -> System.out.println(p.getNotificationType()+":"+p.isNotice()));
     }
 
     public void cancel() {
-        
+        System.out.println(""+previousConfig.size());
+        for(int i = 1;i < previousConfig.size();i++){
+            toggleButtonSelectedproperties.get(i).setValue(previousConfig.get(i));
+            System.out.println("toggleButton"+i+":"+previousConfig.get(i));
+        }
+        save();
     }
 }
